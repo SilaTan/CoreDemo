b4c05d97fe8678b7385ea80dd43d56df5c9fc97c
sila04tan@hotmail.com <sila04tan@hotmail.com>
2021-10-04 15:26:39 +0300

    Yeni Component ve İlişkiler

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator:AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazarın adı soyadı boş geçilemez.");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Yazarın adı en az 2 harf olmalı.");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Yazarın adı en fazla 50 harf olmalı.");
+
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Yazarın maili boş geçilemez.");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Yazarın şifresi boş geçilemez.");
+        }
+    }
+}
--- DataAccessLayer/Migrations/20211004103932_mig_blog_and_writer.Designer.cs
+++ DataAccessLayer/Migrations/20211004103932_mig_blog_and_writer.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20211004103932_mig_blog_and_writer")]
+    partial class mig_blog_and_writer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
